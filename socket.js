3d97ad72f4a04135e59d4645f5aaaf411d27e887
Fang Hai Sen <hansonfang1012@gmail.com>
2026-02-12 12:24:43 -0800

    Fixed getRooms again

--- socket.js
+++ socket.js
@@ -209,7 +209,7 @@ io.on("connection", (socket) => {
    }
  });
  socket.on("getRooms", (data) => {
-     socket.to(roomName).emit("gotRooms", {
+     socket.to("NONE").emit("gotRooms", {
        id: socket.id,
        roomData: {...rooms},
      });
